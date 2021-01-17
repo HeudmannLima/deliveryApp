d9b82347b004484e61598dfe77d23ccfb1cc37d3
Heudmann Lima <heudmannlima@gmail.com>
2021-01-17 13:19:47 -0400

    6 - POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -18,12 +22,30 @@ public class OrderController {
 	@Autowired
 	private OrderService service;
 	
+	
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> getOrders() {
 		List<OrderDTO> list = service.buscarOrdens();
 		
 		// aqui vai retornar GET a lista dos itens
 		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	// Como o que vem do FRONTEND vai ser um JSON contendo os dados de Order,
+	// é necessario usar o @RequestBody pois assim vai converter o body json pra objeto java (dto)
+	@PostMapping
+	public ResponseEntity<OrderDTO> postOrder(@RequestBody OrderDTO dto) {
+		dto = service.inserirOrdem(dto);
+		
+		// Boas praticas: qdo se INSERE/POST um recurso o certo é retornar cod.201 (recurso criado)
+		// pra poder retornar o codigo 201, e tambem o endereço criado no HEAD da resposta, usa URI
+		// aqui é uma chamada que se faz pra criar a URI que corresponde ao RECURSO do post que criamos
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(dto.getId()).toUri();
+		
+		// pra retornar a resposta 201, passa a URI criada, e por fim passa o objeto(body) criado
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,58 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
-	private OrderRepository repository;
+	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	// metodo pra buscar ordens (GET)
 	@Transactional(readOnly = true)
 	public List<OrderDTO> buscarOrdens() {
-		List <Order> list = repository.findOrdersWithProducts();
+		List <Order> list = orderRepository.findOrdersWithProducts();
 	
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	// metodo pra INSERIR ordens (POST)
+	@Transactional
+	public OrderDTO inserirOrdem(OrderDTO dto) {
+		
+		// pega-se todo o objeto modelo de uma ordem, pra assim 
+		// modelar e inserir uma nova com os dados novos
+		Order newOrder = new Order(
+				null, 
+				dto.getAddress(), 
+				dto.getLatitude(), 
+				dto.getLongitude(),
+				Instant.now(),
+				OrderStatus.PENDING);
+		
+		// detalhes: como estou criando num NOVO order, o timestamp é o de agora (Instant.now(),
+		// e a ordemStatus como e um NOVO pedido, ele vai começar como PENDENTE
+		// Abaixo, pra pegar a lista de produtos associados, passo por cada produto do DTO
+		// e ADD ele na lista do tipo produto na minha newOrder
+		for (ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			
+			newOrder.getProducts().add(product);
+		}
+		
+		// SALVANDO essa alteração/nova order no meu BD
+		newOrder = orderRepository.save(newOrder);
+
+		// aqui o meu RETORNO do POST é exatamente a nova ordem criada
+		return new OrderDTO(newOrder);
 	}
 }
