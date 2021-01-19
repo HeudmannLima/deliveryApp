5139ddfefd94f2ccd530838b9b1003c8fb949dbe
Heudmann Lima <heudmannlima@gmail.com>
2021-01-18 22:16:41 -0400

    6. Add ProductCards

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,3 +1,4 @@
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
 
@@ -5,7 +6,7 @@ function Orders() {
   return (
     <div className="orders-container">
       <StepsHeader />
-      <h1>orders</h1>
+      <ProductsList />
     </div>
   )
 }
