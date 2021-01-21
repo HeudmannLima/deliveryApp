df5f7142fa41eec5d15c28d7ad6fa8b8d630b739
Heudmann Lima <heudmannlima@gmail.com>
2021-01-21 16:41:52 -0400

    Fix: Add TOTAL value to payload

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -20,6 +20,7 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 	
 	// Vamos chamar ProductDTO para puxar também as descrições/names
 	// de cada produto quando se puxa as ordens, pois as ordens no Front aparece
@@ -30,13 +31,15 @@ public class OrderDTO implements Serializable {
 	public OrderDTO() {
 	}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, 
+			OrderStatus status, Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 	
 	public OrderDTO(Order entity) {
@@ -46,6 +49,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		// Pega o DTO dos products e cvt pra lista
 		products = entity.getProducts().stream()
 				.map(x -> new ProductDTO(x))
@@ -98,6 +102,15 @@ public class OrderDTO implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public List<ProductDTO> getProducts() {
