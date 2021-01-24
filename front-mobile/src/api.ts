0f0d01d26fdd7b48dd5905ef229f1bf733d739c6
Heudmann Lima <heudmannlima@gmail.com>
2021-01-24 00:45:19 -0400

    Add Stomp/SocketJS to BE and FE

--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://192.168.0.26:8080";
-// const API_URL = 'https://dsdeliver-heud.herokuapp.com';
+// export const API_URL = "http://192.168.0.26:8080";
+const API_URL = 'https://dsdeliver-heud.herokuapp.com';
 
 export async function fetchOrders() {
   return await axios.get(`${API_URL}/orders`);
